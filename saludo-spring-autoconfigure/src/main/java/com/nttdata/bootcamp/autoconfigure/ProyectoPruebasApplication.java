008a9702dab958adb63b0d1a10e65f63040e3875
FranGC94 <116560890+FranGC94@users.noreply.github.com>
2022-10-26 15:11:55 +0200

    Add files via upload

---
+++ saludo-spring-autoconfigure/src/main/java/com/nttdata/bootcamp/autoconfigure/ProyectoPruebasApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.bootcamp.autoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Getter;
+
+@SpringBootApplication
+public class ProyectoPruebasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPruebasApplication.class, args);
+	}
+
+}
